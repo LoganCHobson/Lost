4464a45a3901edd6af7331e07bfe3e5f615ad3d5
LogansPc <loganhobson31@gmail.com>
2023-04-14 19:18:46 -0500

    PlayerMovement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float jumpForce = 1f;
+    public float movementSpeed = 5.0f;
+    public float mouseSensitivity = 2.0f;
+    public float upDownRange = 60.0f;
+
+    float verticalRotation = 0;
+
+    private Rigidbody rb;
+
+    Camera cam;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = GetComponentInChildren<Camera>();
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        // Get movement input
+        float moveX = Input.GetAxis("Horizontal") * movementSpeed * Time.deltaTime;
+        float moveZ = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
+
+        // Move the player
+        transform.Translate(new Vector3(moveX, 0, moveZ));
+
+        // Get mouse input for camera rotation
+        float rotX = Input.GetAxis("Mouse X") * mouseSensitivity;
+        float rotY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+
+        // Rotate the player horizontally
+        transform.Rotate(0, rotX, 0);
+
+        // Rotate the camera vertically
+        verticalRotation -= rotY;
+        verticalRotation = Mathf.Clamp(verticalRotation, -upDownRange, upDownRange);
+        cam.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+
+        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.01f)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
