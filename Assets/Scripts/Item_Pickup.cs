d3173cb4bf3e85894756388a3056cbbbefecd87c
Connor Ballew <conmon119@gmail.com>
2023-04-15 16:49:14 -0500

    Item Dropping Fixes

--- Assets/Scripts/Item_Pickup.cs
+++ Assets/Scripts/Item_Pickup.cs
@@ -7,34 +7,38 @@ public class Item_Pickup : MonoBehaviour
     private Rigidbody rb;
     private SphereCollider sc;
     
-    public bool isHeld;
+    public bool isHeldRight;
+    public bool isHeldLeft;
+
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         sc = GetComponent<SphereCollider>();
       
-        isHeld = false;
+        isHeldRight = false;
+        isHeldLeft = false;
+
     }
 
     private void FixedUpdate()
     {
-        if (isHeld)
+        if (isHeldRight || isHeldLeft)
         {            
             rb.isKinematic = true;
             sc.isTrigger = false;
         }
-        if (Input.GetKey(KeyCode.X) && isHeld)
+        if (Input.GetKey(KeyCode.X) && isHeldRight)
         {
             rb.isKinematic = false;
             sc.isTrigger = true;
-            isHeld = false;
+            isHeldRight = false;
         }
-        if (Input.GetKey(KeyCode.Z) && isHeld)
+        if (Input.GetKey(KeyCode.Z) && isHeldLeft)
         {
             rb.isKinematic = false;
             sc.isTrigger = true;
-            isHeld = false;
+            isHeldLeft = false;
         }
     }
 }
