98ed729aedf1b1d2a6072d23f1c5e5e471cc92a2
DarrelOdom <ddodom17@gmail.com>
2023-04-16 12:12:32 -0500

    Items in scene work

--- Assets/Scripts/Collection.cs
+++ Assets/Scripts/Collection.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Collection : MonoBehaviour
 {
     public int completion;
     public GameObject door;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.name == "Gascan" || other.gameObject.name == "Tire" || other.gameObject.name == "Battery")
@@ -14,12 +17,17 @@ public class Collection : MonoBehaviour
             other.gameObject.GetComponent<Item_Pickup>().isHeldRight = false;
             Destroy(other.gameObject);
             completion++;
+
+            if (completion >= 3)
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
         }
     }
 
     private void Update()
     {
-        if(completion == 3)
+        if(completion >= 3)
         {
             door.SetActive(false);
         }
